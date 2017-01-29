0cbb3363282b6aebeb1248237d05b0ba388272fb
Arsenii Ashukha <ars-ashuha@users.noreply.github.com>
2017-01-29 18:54:03 +0300

    Add files via upload

---
+++ deepbayes-school/2017/task/task.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy import special
+from sklearn.base import ClassifierMixin, BaseEstimator
+from sklearn.datasets import make_classification
+
+# Используйте scipy.special для вычисления численно неустойчивых функций
+# https://docs.scipy.org/doc/scipy/reference/special.html#module-scipy.special
+
+def lossf(w, X, y, l1, l2):
+    """
+    Вычисление функции потерь.
+
+    :param w: numpy.array размера  (M,) dtype = np.float
+    :param X: numpy.array размера  (N, M), dtype = np.float
+    :param y: numpy.array размера  (N,), dtype = np.int
+    :param l1: float, l1 коэффициент регуляризатора 
+    :param l2: float, l2 коэффициент регуляризатора 
+    :return: float, value of loss function
+    """
+    lossf = # Вам необходимо вычислить значение функции потерь тут, решение может занимать 1 строку
+    return lossf
+
+def gradf(w, X, y, l1, l2):
+    """
+    Вычисление градиента функции потерь.
+
+    :param w: numpy.array размера  (M,), dtype = np.float
+    :param X: numpy.array размера  (N, M), dtype = np.float
+    :param y: numpy.array размера  (N,), dtype = np.int
+    :param l1: float, l1 коэффициент регуляризатора 
+    :param l2: float, l2 коэффициент регуляризатора 
+    :return: numpy.array размера  (M,), dtype = np.float, gradient vector d lossf / dw
+    """
+    gradw = # Вам необходимо вычислить градиент функции потерь тут, решение может занимать 1 строку
+    return gradw
+
+class LR(ClassifierMixin, BaseEstimator):
+    def __init__(self, lr=1, l1=1e-4, l2=1e-4, num_iter=1000, verbose=0):
+        self.l1 = l1
+        self.l2 = l2
+        self.w = None
+        self.lr = lr
+        self.verbose = verbose
+        self.num_iter = num_iter
+
+    def fit(self, X, y):
+        """
+        Обучение логистической регрессии.
+        Настраивает self.w коэффициенты модели.
+
+        Если self.verbose == True, то выводите значение 
+        функции потерь на итерациях метода оптимизации. 
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :param y: numpy.array размера  (N,), dtype = np.int
+        :return: self
+        """
+        n, d = X.shape
+        self.w = # Задайте начальное приближение вектора весов
+        self.w = # Настройте параметры функции потерь с помощью градиентного спуска
+        return self
+
+    def predict_proba(self, X):
+        """
+        Предсказание вероятности принадлежности объекта к классу 1.
+        Возвращает np.array размера (N,) чисел в отрезке от 0 до 1.
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :return: numpy.array размера  (N,), dtype = np.int
+        """
+        # Вычислите вероятности принадлежности каждого 
+        # объекта из X к положительному классу, используйте
+        # эту функцию для реализации LR.predict
+        probs = # ...
+        return probs
+
+    def predict(self, X):
+        """
+        Предсказание класса для объекта.
+        Возвращает np.array размера (N,) элементов 1 или -1.
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :return:  numpy.array размера  (N,), dtype = np.int
+        """
+        # Вычислите предсказания для каждого объекта из X
+        predicts = # ...
+        return predicts 
+
+def test_work():
+    print "Start test"
+    X, y = make_classification(n_features=100, n_samples=1000)
+    y = 2 * (y - 0.5)
+
+    try:
+        clf = LR(lr=1, l1=1e-4, l2=1e-4, num_iter=1000, verbose=0)
+    except Exception:
+        assert False, "Создание модели завершается с ошибкой"
+        return
+
+    try:
+        clf = clf.fit(X, y)
+    except Exception:
+        assert False, "Обучение модели завершается с ошибкой"
+        return
+
+    assert isinstance(lossf(clf.w, X, y, 1e-3, 1e-3), float), "Функция потерь должна быть скалярной и иметь тип np.float"
+    assert gradf(clf.w, X, y, 1e-3, 1e-3).shape == (100,), "Размерность градиента должна совпадать с числом параметров"
+    assert gradf(clf.w, X, y, 1e-3, 1e-3).dtype == np.float, "Вектор градиента, должен состоять из элементов типа np.float"
+    assert clf.predict(X).shape == (1000,), "Размер вектора предсказаний, должен совпадать с количеством объектов"
+    assert np.min(clf.predict_proba(X)) >= 0, "Вероятности должны быть не отрицательные"
+    assert np.max(clf.predict_proba(X)) <= 1, "Вероятности должны быть не отрицательные"
+    assert len(set(clf.predict(X))) == 2, "Метод предсказывает больше чем 2 класса на двух классовой задаче"
+    print "End tests"
+
+test_work()
